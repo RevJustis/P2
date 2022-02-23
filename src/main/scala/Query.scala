d4be6fb1ab00a90b71e759d3a1855be9adceeee0
Jessica Hollis <jessica.hollis@revature.net>
2022-02-22 20:36:50 -0500

    Updated code to put parquet files in input directory.

--- src/main/scala/Query.scala
+++ src/main/scala/Query.scala
@@ -15,6 +15,7 @@ object Query {
     val sum2 = ru.groupBy("YEAR").agg(functions.sum("FATALS").as("SUM"))
     sum.orderBy(functions.col("SUM").desc).show(60)
   }
+
   def urban(): Unit = {
     val ur = mainPF.where("A_RU == 2")
 
@@ -27,6 +28,7 @@ object Query {
     sum.orderBy(functions.col("SUM").desc).show(60)
 
   }
+
   def other(): Unit = {
     val other = mainPF.where("A_RU == 3")
 
@@ -39,6 +41,7 @@ object Query {
     sum.orderBy(functions.col("SUM").desc).show(60)
 
   }
+
   def pedal(): Unit = {
     val pedal = mainPF.where("A_PEDAL_F == 1")
 
@@ -145,7 +148,6 @@ object Query {
   }
 
   def age(): Unit = {
-
     println("Pedestrian INJURIES (fatal and nonfatal) by age: ")
 
     val t1 = AgeSexPF.where("AGE<=15")
@@ -156,67 +158,10 @@ object Query {
     val t6 = AgeSexPF.where("AGE<=59 AND AGE>=50")
     val t7 = AgeSexPF.where("AGE<=60 AND AGE>=60")
     val t8 = AgeSexPF.where("AGE>=70")
-    t1
-      .agg(
-        functions
-          .count("*")
-          .as("0-15 years")
-      )
-      .show()
-    t2
-      .agg(
-        functions
-          .count("*")
-          .as("16-23 years")
-      )
-      .show()
-    t3
-      .agg(
-        functions
-          .count("*")
-          .as("24-29 years")
-      )
-      .show()
-
-    t4
-      .agg(
-        functions
-          .count("*")
-          .as("30-39 years")
-      )
-      .show()
-
-    t5
-      .agg(
-        functions
-          .count("*")
-          .as("40-49 years")
-      )
-      .show()
-
-    t6
-      .agg(
-        functions
-          .count("*")
-          .as("50-59 years")
-      )
-      .show()
-
-    t7
-      .agg(
-        functions
-          .count("*")
-          .as("60-69 years")
-      )
-      .show()
-
-    t8
-      .agg(
-        functions
-          .count("*")
-          .as("70+ years")
-      )
-      .show()
+    val x: List[(DataFrame, String)] =
+      List((t1, "0-15 years"), (t2, "16-23"), (t3, "24-29"), (t4, "30-39"),
+        (t5, "40-49"), (t6, "50-59"), (t7, "60-69"), (t8, "70+"))
+    x.foreach(tup => tup._1.agg(functions.count("*").as(tup._2)).show())
 
     println("Pedestrian FATALITIES by age: ")
     val t9 = AgeSexPF.where("AGE<=15")
@@ -228,184 +173,116 @@ object Query {
     val t15 = AgeSexPF.where("AGE<=60 AND AGE>=60")
     val t16 = AgeSexPF.where("AGE>=70")
     val l: List[(DataFrame, String)] =
-      List((t9, "0-15 years"), (t10, "16-23"))
+      List((t9, "0-15 years AND A_PED_F"), (t10, "16-23 AND A_PED_F"), (t11, "24-29 AND A_PED_F"), (t12, "30-39 AND A_PED_F"),
+        (t13, "40-49 AND A_PED_F"), (t14, "50-59 AND A_PED_F"), (t15, "60-69 AND A_PED_F"), (t16, "70+ AND A_PED_F"))
     l.foreach(tup => tup._1.agg(functions.count("*").as(tup._2)).show())
-
-    // val t9 = AgeSexPF.where("AGE<=15 AND )
-    // val t10 = AgeSexPF.where("AGE<=23 AND AGE>=16")
-    // val t11 = AgeSexPF.where("AGE<=29 AND AGE>=24")
-    // val t12 = AgeSexPF.where("AGE<=39 AND AGE>=30")
-    // val t13 = AgeSexPF.where("AGE<=49 AND AGE>=40")
-    // val t14 = AgeSexPF.where("AGE<=59 AND AGE>=50")
-    // val t15 = AgeSexPF.where("AGE<=60 AND AGE>=60")
-    // val t16 = AgeSexPF.where("AGE>=70")
-    //
-    // t9
-    // .agg(
-    // functions
-    // .count("*")
-    // .as("0-15 years")
-    // )
-    // .show()
-    // t10
-    // .agg(
-    // functions
-    // .count("*")
-    // .as("16-23 years")
-    // )
-    // .show()
-    // t11
-    // .agg(
-    // functions
-    // .count("*")
-    // .as("24-29 years")
-    // )
-    // .show()
-    //
-    // t12
-    // .agg(
-    // functions
-    // .count("*")
-    // .as("30-39 years")
-    // )
-    // .show()
-    //
-    // t13
-    // .agg(
-    // functions
-    // .count("*")
-    // .as("40-49 years")
-    // )
-    // .show()
-
-    // t14
-    // .agg(
-    // functions
-    // .count("*")
-    // .as("50-59 years")
-    // )
-    // .show()
-    //
-    // t15
-    // .agg(
-    // functions
-    // .count("*")
-    // .as("60-69 years")
-    // )
-    // .show()
-
-    // t16
-    // .agg(
-    // functions
-    // .count("*")
-    // .as("70+ years")
-    // )
-    // .show()
-  }
-
-  def jonathan(): Unit = {
-    spark.sql("select * from personskilled").show()
-    spark
-      .sql(
-        "select year, passengerCars, buses, total1 as TotalExcludingMotorcyclesAndPed, " +
-          "motorcycles as Delta, (total1 + motorcycles) as TotalExcludingPed, abs((total1 + motorcycles) - total) as DeltaPED," +
-          " total from personsKilled where year between 2008 and 2018"
-      )
-      .show()
-  }
-
-  ///Begin Patrick
-
-  def usfatals(): Unit = {
-    //Graph the trend of fatalities in the entire USA
-    println("Trend of fatalities in the entire USA from 2016 to 2019:")
-    val dfAllUS = spark.sql(
-      "SELECT sum(fatals) as fatalities, year from crashData group by year order by year"
-    )
-    dfAllUS.persist(StorageLevel.MEMORY_ONLY_SER)
-    dfAllUS.show()
-  }
-
-  def statefatals(): Unit = {
-    //Graph the trend of fatalities in individual states
-    println("Trend of fatalities in individual states from 2016 to 2019:")
-    val dfState = spark.sql(
-      "SELECT sum(fatals) as fatalities, year, state from crashData group by state, year \n" +
-        "order by state, year"
-    )
-    dfState.persist(StorageLevel.MEMORY_ONLY_SER)
-    dfState.show()
-  }
-
-  def highfatalstates(): Unit = {
-    //Trend by year for all states
-    //States with highest crashes every year
-    println("States with highest crash fatality numbers every year: ")
-    val dfState2016 = spark.sql(
-      "SELECT sum(fatals) as fatalities, year, state from crashData where year = 2016 \n" +
-        "group by state, year order by fatalities DESC LIMIT 8"
-    )
-    val dfState2017 = spark.sql(
-      "SELECT sum(fatals) as fatalities, year, state from crashData where year = 2017\n" +
-        "group by state, year order by fatalities DESC LIMIT 8"
-    )
-    val dfState2018 = spark.sql(
-      "SELECT sum(fatals) as fatalities, year, state from crashData where year = 2018 \n" +
-        "group by state, year order by fatalities DESC LIMIT 8"
-    )
-    val dfState2019 = spark.sql(
-      "SELECT sum(fatals) as fatalities, year, state from crashData where year = 2019 \n" +
-        "group by state, year order by fatalities DESC LIMIT 8"
-    )
-    //Optimization
-    dfState2016.persist(StorageLevel.MEMORY_ONLY_SER)
-    dfState2016.persist(StorageLevel.MEMORY_ONLY_SER)
-    dfState2016.persist(StorageLevel.MEMORY_ONLY_SER)
-    dfState2016.persist(StorageLevel.MEMORY_ONLY_SER)
-    dfState2016.show()
-    dfState2017.show()
-    dfState2018.show()
-    dfState2019.show()
-  }
-
-  def lowfatalstates(): Unit = {
-    //States with lowest crashes every year
-    println("States with lowest crash fatality numbers every year: ")
-    val state2016down = spark.sql(
-      "SELECT sum(fatals) as fatalities, year, state from crashData where year = 2016 \n" +
-        "group by state, year order by fatalities LIMIT 8"
-    )
-    val state2017down = spark.sql(
-      "SELECT sum(fatals) as fatalities, year, state from crashData where year = 2017\n" +
-        "group by state, year order by fatalities LIMIT 8"
-    )
-    val state2018down = spark.sql(
-      "SELECT sum(fatals) as fatalities, year, state from crashData where year = 2018 \n" +
-        "group by state, year order by fatalities LIMIT 8"
-    )
-    val state2019down = spark.sql(
-      "SELECT sum(fatals) as fatalities, year, state from crashData where year = 2019 \n" +
-        "group by state, year order by fatalities LIMIT 8"
-    )
-    //Optimization
-    state2016down.persist(StorageLevel.MEMORY_ONLY_SER)
-    state2017down.persist(StorageLevel.MEMORY_ONLY_SER)
-    state2018down.persist(StorageLevel.MEMORY_ONLY_SER)
-    state2019down.persist(StorageLevel.MEMORY_ONLY_SER)
-    state2016down.show()
-    state2017down.show()
-    state2018down.show()
-    state2019down.show()
-  }
-
-  def vehicleCrash(): Unit = {
-    //See what type of vehicle led to the most crashes.
-    println("Here are the stats for different vehicles: ")
-    val x = spark.sql(
-      "select * from vehicleParquetFile order by Year, VehicleType"
-    )
-    //Optimization
-    x.persist(StorageLevel.MEMORY_ONLY_SER)
-    x.show(28)
-  }
 }
+
+
+    def jonathan(): Unit = {
+      spark.sql("select * from personskilled").show()
+      spark
+        .sql(
+          "select year, passengerCars, buses, total1 as TotalExcludingMotorcyclesAndPed, " +
+            "motorcycles as Delta, (total1 + motorcycles) as TotalExcludingPed, abs((total1 + motorcycles) - total) as DeltaPED," +
+            " total from personsKilled where year between 2008 and 2018"
+        )
+        .show()
+    }
+
+    ///Begin Patrick
+
+    def usfatals(): Unit = {
+      //Graph the trend of fatalities in the entire USA
+      println("Trend of fatalities in the entire USA from 2016 to 2019:")
+      val dfAllUS = spark.sql(
+        "SELECT sum(fatals) as fatalities, year from crashData group by year order by year"
+      )
+      dfAllUS.persist(StorageLevel.MEMORY_ONLY_SER)
+      dfAllUS.show()
+    }
+
+    def statefatals(): Unit = {
+      //Graph the trend of fatalities in individual states
+      println("Trend of fatalities in individual states from 2016 to 2019:")
+      val dfState = spark.sql(
+        "SELECT sum(fatals) as fatalities, year, state from crashData group by state, year \n" +
+          "order by state, year"
+      )
+      dfState.persist(StorageLevel.MEMORY_ONLY_SER)
+      dfState.show()
+    }
+
+    def highfatalstates(): Unit = {
+      //Trend by year for all states
+      //States with highest crashes every year
+      println("States with highest crash fatality numbers every year: ")
+      val dfState2016 = spark.sql(
+        "SELECT sum(fatals) as fatalities, year, state from crashData where year = 2016 \n" +
+          "group by state, year order by fatalities DESC LIMIT 8"
+      )
+      val dfState2017 = spark.sql(
+        "SELECT sum(fatals) as fatalities, year, state from crashData where year = 2017\n" +
+          "group by state, year order by fatalities DESC LIMIT 8"
+      )
+      val dfState2018 = spark.sql(
+        "SELECT sum(fatals) as fatalities, year, state from crashData where year = 2018 \n" +
+          "group by state, year order by fatalities DESC LIMIT 8"
+      )
+      val dfState2019 = spark.sql(
+        "SELECT sum(fatals) as fatalities, year, state from crashData where year = 2019 \n" +
+          "group by state, year order by fatalities DESC LIMIT 8"
+      )
+      //Optimization
+      dfState2016.persist(StorageLevel.MEMORY_ONLY_SER)
+      dfState2016.persist(StorageLevel.MEMORY_ONLY_SER)
+      dfState2016.persist(StorageLevel.MEMORY_ONLY_SER)
+      dfState2016.persist(StorageLevel.MEMORY_ONLY_SER)
+      dfState2016.show()
+      dfState2017.show()
+      dfState2018.show()
+      dfState2019.show()
+    }
+
+    def lowfatalstates(): Unit = {
+      //States with lowest crashes every year
+      println("States with lowest crash fatality numbers every year: ")
+      val state2016down = spark.sql(
+        "SELECT sum(fatals) as fatalities, year, state from crashData where year = 2016 \n" +
+          "group by state, year order by fatalities LIMIT 8"
+      )
+      val state2017down = spark.sql(
+        "SELECT sum(fatals) as fatalities, year, state from crashData where year = 2017\n" +
+          "group by state, year order by fatalities LIMIT 8"
+      )
+      val state2018down = spark.sql(
+        "SELECT sum(fatals) as fatalities, year, state from crashData where year = 2018 \n" +
+          "group by state, year order by fatalities LIMIT 8"
+      )
+      val state2019down = spark.sql(
+        "SELECT sum(fatals) as fatalities, year, state from crashData where year = 2019 \n" +
+          "group by state, year order by fatalities LIMIT 8"
+      )
+      //Optimization
+      state2016down.persist(StorageLevel.MEMORY_ONLY_SER)
+      state2017down.persist(StorageLevel.MEMORY_ONLY_SER)
+      state2018down.persist(StorageLevel.MEMORY_ONLY_SER)
+      state2019down.persist(StorageLevel.MEMORY_ONLY_SER)
+      state2016down.show()
+      state2017down.show()
+      state2018down.show()
+      state2019down.show()
+    }
+
+    def vehicleCrash(): Unit = {
+      //See what type of vehicle led to the most crashes.
+      println("Here are the stats for different vehicles: ")
+      val x = spark.sql(
+        "select * from vehicleParquetFile order by Year, VehicleType"
+      )
+      //Optimization
+      x.persist(StorageLevel.MEMORY_ONLY_SER)
+      x.show(28)
+    }
+  }
+
